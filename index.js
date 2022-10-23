997c4f5618c0d2305a885d3c6dd17c9846a0a49c
marshall <mhamad.marshall@gmail.com>
2022-10-23 20:36:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+
+  return tutorials.map(str => {
+    str = str.split(' ');
+    for (let i=0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+    return str.join(' ');
+  })
 }
